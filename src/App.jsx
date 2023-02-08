7689a8608f1dc0154f521261e8d223760a704ca6
dqarias <dino.quispea@gmail.com>
2023-02-08 15:52:12 -0500

    Set up linters eslint and stylelint

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import "./App.css";
-
 function App() {
   return (
     <div className="App">
       <div>Hello World</div>
+      <div />
     </div>
   );
 }
