86e864540ce08913c2ace8b9f5df094666483b20
dqarias <dino.quispea@gmail.com>
2023-02-08 16:29:53 -0500

    add functionality to consume the store

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
+import { Provider } from 'react-redux';
+import store from './app/store';
+
 function App() {
   return (
-    <div className="App">
-      <div>Hello World</div>
-      <div />
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <div>Hello World</div>
+        <div />
+      </div>
+    </Provider>
+
   );
 }
 
